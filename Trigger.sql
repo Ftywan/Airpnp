1d522fe4d9c575ca00ee214db4da30675b5f6844
treetree33 <e0014934@u.nus.edu>
2019-03-31 17:06:55 +0800

    Refined schema 5.0 & refined triggers

--- Trigger.sql
+++ Trigger.sql
@@ -4,8 +4,8 @@ RETURNS TRIGGER AS $$
 DECLARE
 		count integer;
 BEGIN
-		SELECT COUNT(*) INTO count FROM BiddedHost b where b.ownerName = NEW.ownerName 
-		and b.petName = NEW.petName and b.startdate = NEW.startdate and b.enddate = NEW.enddate;
+		SELECT COUNT(*) INTO count FROM Not_completed_accommodation n where n.ownerName = NEW.ownerName 
+		and NEW.petName in (select petName from Not_completed_accommodation) and ((n.startdate <= NEW.startdate and n.enddate >= NEW.enddate) or (n.startdate < NEW.enddate and NEW.enddate < n.enddate) or (NEW.startdate > n.startdate and NEW.startdate < n.enddate);
 		If count > 0 THEN
 				RAISE EXCEPTION 'This service is confirmed. No further bidding on this service is allowed.';
 				RETURN NULL;
@@ -17,7 +17,7 @@ LANGUAGE PLPGSQL;
 -- Check whether the new bidding is created/updated after checking that there is no duplicate bidding on the already exist services.
 CREATE TRIGGER check_for_further_bidding
 BEFORE INSERT OR UPDATE
-ON OwnerStatus
+ON BiddingStatus
 FOR EACH ROW
 EXECUTE PROCEDURE forbid_next_bidding();
 
@@ -28,8 +28,8 @@ RETURN TRIGGER AS $$
 DECLARE
 		count integer;
 BEGIN 
-		SELECT COUNT(*) INTO count FROM TimeFrame t where t.startdate <= NEW.startdate and t.enddate >= NEW.enddate 
-		and NEW.bids < t.minBid;
+		SELECT COUNT(*) INTO count FROM Services s where s.startdate <= NEW.startdate and s.enddate >= NEW.enddate 
+		and NEW.bids < s.minBid;
 		IF COUNT > 0 THEN 
 				RAISE EXCEPTION 'This bid is too little to be made.';
 				RETURN NULL;
@@ -41,7 +41,7 @@ LANGUAGE PLPGSQL;
 ---Check whether the bids is above the min.
 CREATE TRIGGER check_for_min_bid
 BEFORE INSERT OR UPDATE
-ON Bidders
+ON BiddingStatus
 FOR EACH ROW
 EXECUTE PROCEDURE forbid_little_bids();
 
@@ -51,12 +51,10 @@ CREATE OR REPLACE FUNCTION forbid_over_capacity()
 RETURN TRIGGER AS $$
 DECLARE 
 		count integer;
-		capacity integer;
 BEGIN
-		SELECT COUNT(*) INTO count FROM CareTakerStatus c where (
-			SELECT(c.startdate, c.enddate) OVERLAPS (NEW.startdate, NEW.enddate));
-		SELECT c1.num INTO capacity FROM Capacity c1;
-		IF (NEW.sendingNum + count > capacity) THEN
+		SELECT COUNT(*) INTO count FROM Services s where (
+			s.capacity < count(NEW.petName)
+		IF (count > 0 ) THEN
 				RAISE EXCEPTION 'cannot accommodate so many pets.';
 				RETURN NULL;
 		END IF;
@@ -67,7 +65,7 @@ LANGUAGE PLPGSQL
 -- check whether the bids can be made according to capacity
 CREATE TRIGGER check_for_capacity
 BEFORE INSERT OR UPDATE
-ON Bidders
+ON BiddingStatus
 FOR EACH ROW
 EXECUTE PROCEDURE forbid_over_capacity();
 
