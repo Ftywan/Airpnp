99276d31753cdccf69f52ecb16df64363ee1ae5b
JR <jingrulin@u.nus.edu>
2019-04-01 22:12:17 +0800

    test data 1

---
+++ test.sql
@@ -0,0 +1,122 @@
+-- Users table
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('jkalkhoven0', 6, '71oJZpQIHR', 3, '320 Lotheville Pass', '6326062455', 'Aliquam quis turpis eget elit sodales scelerisque.', 'Sed sagittis.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('mdowdall1', 7, 'SaTJYEoPDSa', 8, '86196 Tennyson Place', '5608495262', 'Morbi vel lectus in quam fringilla rhoncus.', 'Praesent lectus.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('hdalliwater2', 4, '91eh9le', 6, '59 Hagan Lane', '1878628621', 'Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.', 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('dstarling3', 7, 'd8UWFJp', 1, '075 Crowley Junction', '3114314284', 'Nulla tellus.', 'Proin eu mi.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('jchaudrelle4', 6, 'UDtaen19Y', 8, '16 Merry Street', '5739770769', 'In hac habitasse platea dictumst.', 'In hac habitasse platea dictumst.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('mcrammy5', 9, 'LqBHqq', 8, '0846 Ridgeview Court', '6287553911', 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.', 'Praesent blandit.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('pgaythor6', 10, 'YSq3zvEXHv', 2, '4 Rowland Court', '4864745813', 'Aenean lectus.', 'Aenean lectus.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('ocornier7', 8, 'wXfeDnd', 1, '258 Emmet Court', '2095797714', 'Nulla facilisi.', 'Maecenas rhoncus aliquam lacus.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('csunnex8', 3, 'AuoND4', 1, '50 Reinke Parkway', '6853374155', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.', 'Suspendisse potenti.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('scabedo9', 3, 'GyDtI1UX9gLm', 7, '7 Myrtle Parkway', '1888500912', 'Etiam vel augue.', 'Praesent id massa id nisl venenatis lacinia.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('cmcquilkina', 3, 'BFYzHvCr8bwf', 10, '4618 Oak Park', '5826298909', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 'Curabitur convallis.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('hhallattb', 5, 'NzgF59', 6, '07613 Clarendon Parkway', '3861417769', 'Fusce consequat.', 'Praesent lectus.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('lworboyc', 8, 'rY2wcu6iqB', 5, '8 Mitchell Court', '6861099773', 'Etiam pretium iaculis justo.', 'Suspendisse potenti.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('tbeatend', 5, 'e3N3f7JmsM', 4, '5304 Muir Alley', '2346335952', 'Praesent lectus.', 'Morbi non quam nec dui luctus rutrum.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('fwaighte', 5, 'lIYJA4DhO', 4, '985 8th Avenue', '6075463911', 'Mauris lacinia sapien quis libero.', 'Morbi vel lectus in quam fringilla rhoncus.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('hyairf', 4, 'U2N4FK', 10, '5 Westerfield Drive', '7881672777', 'Fusce consequat.', 'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('spawlatag', 10, '5YzqvsO7', 5, '0778 Pleasure Lane', '6607882939', 'Sed accumsan felis.', 'Etiam pretium iaculis justo.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('edefriesh', 7, 'lRQIG0OJmgM', 1, '2 Lawn Road', '5117255545', 'Proin risus.', 'Duis consequat dui nec nisi volutpat eleifend.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('groseamani', 10, 'ChCX876NH5vM', 1, '13 Artisan Street', '7427460876', 'Morbi porttitor lorem id ligula.', 'Vivamus vel nulla eget eros elementum pellentesque.');
+insert into Users (username, rating, password, numPets, location, contact_number, verification_qn, answer) values ('bmctrusteyj', 6, 'FRRrhumD', 6, '5334 Bartillon Court', '7957476774', 'Sed vel enim sit amet nunc viverra dapibus.', 'Integer a nibh.');
+
+-- PetOwners
+insert into PetOwners (username) values ('shodgins0');
+insert into PetOwners (username) values ('amemory1');
+insert into PetOwners (username) values ('icouve2');
+insert into PetOwners (username) values ('cwinscom3');
+insert into PetOwners (username) values ('merrowe4');
+insert into PetOwners (username) values ('blippingwell5');
+insert into PetOwners (username) values ('cwingeatt6');
+insert into PetOwners (username) values ('sburehill7');
+insert into PetOwners (username) values ('skalaher8');
+insert into PetOwners (username) values ('bpassfield9');
+
+-- Pets
+insert into Pets (petName, petType, ownerName) values ('gfearnyhough0', 'Hummingbird (unidentified)', 'cgehrtz0');
+insert into Pets (petName, petType, ownerName) values ('mhukin1', 'Crab (unidentified)', 'fcaplen1');
+insert into Pets (petName, petType, ownerName) values ('crieger2', 'Gull, southern black-backed', 'goscollain2');
+insert into Pets (petName, petType, ownerName) values ('roakey3', 'Snake, tiger', 'kjouanny3');
+insert into Pets (petName, petType, ownerName) values ('jertel4', 'Pied crow', 'lbazoge4');
+insert into Pets (petName, petType, ownerName) values ('rtubridy5', 'Paca', 'dperelli5');
+insert into Pets (petName, petType, ownerName) values ('fcorah6', 'Blue crane', 'cbenardeau6');
+insert into Pets (petName, petType, ownerName) values ('blowman7', 'Duiker, common', 'tbrader7');
+insert into Pets (petName, petType, ownerName) values ('uchamperlen8', 'Badger, european', 'cpetrelluzzi8');
+insert into Pets (petName, petType, ownerName) values ('cstead9', 'Striped skunk', 'epartington9');
+insert into Pets (petName, petType, ownerName) values ('jdinjesa', 'Possum, golden brush-tailed', 'pmcalorena');
+insert into Pets (petName, petType, ownerName) values ('bloinesb', 'Australian spiny anteater', 'akillingbeckb');
+insert into Pets (petName, petType, ownerName) values ('sodunniomc', 'Puna ibis', 'bhamsherc');
+insert into Pets (petName, petType, ownerName) values ('hcrutchleyd', 'Salmon, sockeye', 'tnoored');
+insert into Pets (petName, petType, ownerName) values ('jbriggdalee', 'Pine squirrel', 'dinghame');
+insert into Pets (petName, petType, ownerName) values ('emccrowf', 'Nyala', 'emelinf');
+insert into Pets (petName, petType, ownerName) values ('ekerg', 'Rock dove', 'cabsolong');
+insert into Pets (petName, petType, ownerName) values ('awilseh', 'Eastern grey kangaroo', 'cbreslauh');
+insert into Pets (petName, petType, ownerName) values ('olimeburneri', 'Common green iguana', 'leallesi');
+insert into Pets (petName, petType, ownerName) values ('rkyrkemanj', 'Marmot, yellow-bellied', 'rwabbj');
+
+-- Favorite
+insert into Favorite (ownerName, hostName) values ('relbourn0', 'fmeadows0');
+insert into Favorite (ownerName, hostName) values ('gchatburn1', 'dfulep1');
+insert into Favorite (ownerName, hostName) values ('mskough2', 'kbache2');
+insert into Favorite (ownerName, hostName) values ('rkeeves3', 'ferni3');
+insert into Favorite (ownerName, hostName) values ('dinchbald4', 'aolivia4');
+insert into Favorite (ownerName, hostName) values ('klaweles5', 'ahuonic5');
+insert into Favorite (ownerName, hostName) values ('gvick6', 'taugustine6');
+insert into Favorite (ownerName, hostName) values ('cdallaway7', 'efrondt7');
+insert into Favorite (ownerName, hostName) values ('tguillet8', 'ecopson8');
+insert into Favorite (ownerName, hostName) values ('nwavell9', 'florenzo9');
+insert into Favorite (ownerName, hostName) values ('kmurcutta', 'rmorrisona');
+insert into Favorite (ownerName, hostName) values ('dkarolovskyb', 'cballhamb');
+insert into Favorite (ownerName, hostName) values ('hheddenc', 'coliphardc');
+insert into Favorite (ownerName, hostName) values ('iriseboroughd', 'spolkinghorned');
+insert into Favorite (ownerName, hostName) values ('wosticke', 'clockitte');
+insert into Favorite (ownerName, hostName) values ('bridgersf', 'ncawsyf');
+insert into Favorite (ownerName, hostName) values ('slongfootg', 'iborlessg');
+insert into Favorite (ownerName, hostName) values ('wemmeth', 'chalyh');
+insert into Favorite (ownerName, hostName) values ('ydirkini', 'zfaradayi');
+insert into Favorite (ownerName, hostName) values ('ldunbarj', 'lbottej');
+
+-- CareTakers
+insert into CareTakers (username) values ('oaukland0');
+insert into CareTakers (username) values ('tghilardi1');
+insert into CareTakers (username) values ('ngavagan2');
+insert into CareTakers (username) values ('kjerrans3');
+insert into CareTakers (username) values ('lslimming4');
+insert into CareTakers (username) values ('kwinsbury5');
+insert into CareTakers (username) values ('ipersian6');
+insert into CareTakers (username) values ('ogisburne7');
+insert into CareTakers (username) values ('lpiquard8');
+insert into CareTakers (username) values ('osimeons9');
+insert into CareTakers (username) values ('lscoffhama');
+insert into CareTakers (username) values ('ecoulesb');
+insert into CareTakers (username) values ('amarczykc');
+insert into CareTakers (username) values ('rupwardd');
+insert into CareTakers (username) values ('aoflannerye');
+insert into CareTakers (username) values ('myanezf');
+insert into CareTakers (username) values ('dpostleg');
+insert into CareTakers (username) values ('fjaouenh');
+insert into CareTakers (username) values ('tdrippsi');
+insert into CareTakers (username) values ('tduleyj');
+
+
+-- Wishlist
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('dlandon0', 'dwinckles0', '11/13/2018', '4/28/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('cdaniells1', 'rkochl1', '8/22/2018', '6/8/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('shacker2', 'xbrotherhood2', '11/18/2018', '7/16/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('spetters3', 'edavidsohn3', '3/31/2019', '8/19/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('sdennistoun4', 'wabbati4', '2/17/2019', '1/22/2019');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('bcurtis5', 'nfleeman5', '9/12/2018', '12/25/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('okettell6', 'ycrellim6', '2/28/2019', '1/16/2019');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('ejenman7', 'mleverington7', '11/8/2018', '12/10/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('cchalloner8', 'cdukelow8', '5/10/2018', '10/15/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('apietsma9', 'akoubu9', '8/7/2018', '8/22/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('shuygensa', 'bleavensa', '1/31/2019', '5/13/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('wbracherb', 'rcaveneyb', '1/4/2019', '7/13/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('pscogginc', 'avandrillc', '4/15/2018', '1/4/2019');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('mfoldd', 'dgrinikhind', '12/9/2018', '2/21/2019');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('hovesene', 'jheggiee', '7/15/2018', '3/21/2019');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('chandasydef', 'jhausef', '3/12/2019', '12/25/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('mmorotg', 'mzettlerg', '2/27/2019', '9/6/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('amcramseyh', 'ebeaulyh', '5/1/2018', '6/30/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('khurdidgei', 'jchatwoodi', '7/11/2018', '12/5/2018');
+insert into Wishlist (ownerName, hostName, startdate, enddate) values ('jsalvatij', 'iaingellj', '4/12/2018', '9/19/2018');
