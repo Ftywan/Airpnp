b4a5f6262bc4af67ce6410c61d5040be7f133c48
treetree33 <e0014934@u.nus.edu>
2019-04-01 22:22:22 +0800

    test data 2

---
+++ test_data_2.sql
@@ -0,0 +1,91 @@
+---accommodated
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('aclevely0', 'poiller0', 'wmckim0', '12/17/2018', '5/5/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('shelks1', 'mfolomin1', 'sgrealey1', '2/18/2019', '9/4/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('dcleatherow2', 'xjaouen2', 'raleksich2', '10/4/2018', '8/22/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('aboarleyson3', 'jberrane3', 'orosevear3', '9/6/2018', '5/25/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('rrozzell4', 'estrathman4', 'ybleyman4', '12/13/2018', '3/7/2019');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('fflewan5', 'tmiko5', 'vshapcott5', '4/29/2018', '7/30/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('cmunton6', 'bmcintee6', 'mmccutheon6', '11/27/2018', '7/28/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('sgrundy7', 'fwebster7', 'lbratley7', '3/12/2019', '11/18/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('amazzia8', 'coxnam8', 'rdillon8', '3/24/2019', '4/13/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('lcochrane9', 'caddie9', 'mwonham9', '7/30/2018', '5/3/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('cwatheya', 'hsavegea', 'akermatha', '11/12/2018', '7/16/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('cdartanb', 'emulranb', 'lfleetwoodb', '5/1/2018', '1/22/2019');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('ihumberstonec', 'mdarkerc', 'jlambdinc', '9/11/2018', '6/3/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('yinglefieldd', 'kmcelhinneyd', 'cgershomd', '10/13/2018', '4/23/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('pormerode', 'rcrowthee', 'dgarraltse', '9/22/2018', '9/8/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('kburgoynef', 'ysiredf', 'ahounsonf', '1/4/2019', '4/24/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('mbenthallg', 'ffargieg', 'sdoweg', '3/6/2019', '6/11/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('rolennachainh', 'lknotonh', 'bgerglerh', '4/3/2018', '9/28/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('arosenvasseri', 'sstandfieldi', 'uhalmi', '10/4/2018', '6/4/2018');
+insert into Accommodation (hostName, petName, ownerName, startdate, enddate) values ('habdyj', 'amacpakej', 'agotchj', '7/10/2018', '8/30/2018');
+
+-- not_completed_accommodation
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('jwayte0', 'spattison0', 'sdavet0', '7/6/2018', '5/12/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('dvannuchi1', 'trozycki1', 'smilhench1', '12/6/2018', '12/1/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('dcoutthart2', 'mlenney2', 'hdenholm2', '11/5/2018', '11/5/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('tubsdell3', 'nmunn3', 'rswansbury3', '10/20/2018', '1/28/2019');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('pskune4', 'lninotti4', 'ascarasbrick4', '9/22/2018', '5/23/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('mbarbery5', 'lhorsted5', 'epetrescu5', '9/21/2018', '6/21/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('abeasleigh6', 'rfreed6', 'fdeas6', '10/27/2018', '11/18/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('santonioni7', 'jkearey7', 'lmasserel7', '4/29/2018', '10/24/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('ltossell8', 'tcastagna8', 'ccartmale8', '12/29/2018', '7/30/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('abewsy9', 'narmall9', 'tsexon9', '9/22/2018', '5/3/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('mmcgeea', 'crentillla', 'okenninghana', '2/16/2019', '10/8/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('tgiacobiliob', 'sjobernb', 'bkulasb', '12/7/2018', '3/20/2019');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('snaisbittc', 'aeaglenc', 'bstanyforthc', '11/23/2018', '10/22/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('bdebischofd', 'sstrognelld', 'mruselind', '3/11/2019', '5/25/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('kbakese', 'donowlane', 'aclipstone', '1/24/2019', '2/1/2019');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('dgreaderf', 'ecursonf', 'tducrowf', '12/16/2018', '4/26/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('jbeckworthg', 'emacakg', 'rdumbrallg', '3/5/2019', '1/19/2019');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('bgoareh', 'tashpitalh', 'bkinnerleyh', '11/13/2018', '4/29/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('rkaszperi', 'ssalthousei', 'smcsporoni', '12/6/2018', '11/11/2018');
+insert into Not_completed_accommodation (hostName, petName, ownerName, startdate, enddate) values ('cbroszkiewiczj', 'wfortej', 'ajarviej', '6/28/2018', '6/1/2018');
+
+
+--Bidding Status
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('oyousef0', 'kharty0', 714.06, 'jabela0', '5:01', '7/6/2018', '8/17/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('mjanuszewski1', 'tfandrich1', 699.18, 'larendsen1', '19:03', '9/17/2018', '2/4/2019');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('hhennington2', 'kbrend2', 529.84, 'bstutt2', '7:58', '10/1/2018', '11/17/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('gglading3', 'jalgore3', 85.53, 'jridsdell3', '3:18', '7/17/2018', '5/23/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('afetters4', 'gdeangelis4', 308.37, 'byurlov4', '23:34', '4/23/2018', '7/31/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('vhengoed5', 'klambin5', 444.2, 'bfarnsworth5', '13:14', '5/25/2018', '1/23/2019');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('sclaridge6', 'mkirkman6', 69.78, 'nwortt6', '17:02', '11/21/2018', '6/30/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('sscriviner7', 'bpellitt7', 78.09, 'rlowther7', '9:40', '11/16/2018', '8/18/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('jbestman8', 'ispalls8', 197.61, 'peydel8', '1:14', '2/26/2019', '4/4/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('jsmalley9', 'bmactrusty9', 312.27, 'mwinsley9', '5:05', '2/24/2019', '11/26/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('mheinricha', 'fjanskya', 276.87, 'lolmana', '22:20', '5/28/2018', '1/24/2019');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('cthiolierb', 'dbrambleb', 856.67, 'npickthornb', '13:56', '9/8/2018', '6/9/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('fmcveaghc', 'ejizhakic', 105.77, 'dsnooksc', '9:47', '9/29/2018', '8/4/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('drosenbladd', 'rabellad', 222.95, 'csimonettod', '4:55', '8/24/2018', '1/16/2019');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('dbrislane', 'edarkine', 471.98, 'lconnuee', '11:41', '8/12/2018', '5/14/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('kvondrasekf', 'tglanvillf', 676.34, 'mfiddianf', '12:50', '10/13/2018', '10/22/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('gstrittong', 'jebbingg', 195.19, 'segertong', '12:29', '12/26/2018', '10/1/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('mkerseyh', 'mshanksterh', 753.35, 'cgotherh', '4:14', '11/25/2018', '5/27/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('ltytcombi', 'sfishbiei', 681.54, 'cheimsi', '18:41', '6/18/2018', '6/11/2018');
+insert into BiddingStatus (owerName, hostName, bids, petName, create_at, startdate, enddate) values ('vgrollj', 'mhatliffj', 994.93, 'cmarvellj', '8:59', '8/13/2018', '9/10/2018');
+-- need to manually add in the status
+
+
+
+---Services
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('bmattioni0', 920.61, '2/17/2019', '2/14/2019', 8);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('rclashe1', 497.95, '5/3/2018', '7/10/2018', 5);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('aostick2', 681.86, '12/31/2018', '2/24/2019', 6);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('jphilippon3', 956.95, '4/5/2018', '12/16/2018', 5);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('bsenett4', 877.58, '10/24/2018', '11/15/2018', 6);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('jworviell5', 214.78, '4/22/2018', '9/13/2018', 2);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('mdundin6', 355.96, '8/30/2018', '5/19/2018', 6);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('ebodycote7', 455.7, '3/17/2019', '2/16/2019', 6);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('cgookey8', 910.9, '1/6/2019', '8/1/2018', 8);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('flishman9', 286.93, '5/9/2018', '9/30/2018', 1);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('rbutlanda', 565.05, '1/20/2019', '1/14/2019', 10);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('eingerfieldb', 596.98, '1/29/2019', '6/25/2018', 3);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('tgiottoic', 777.33, '1/28/2019', '6/9/2018', 2);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('aculshawd', 153.69, '9/17/2018', '10/27/2018', 1);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('rdefonte', 260.83, '6/14/2018', '10/1/2018', 1);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('elefebref', 355.37, '4/30/2018', '8/1/2018', 1);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('tberreg', 528.38, '10/7/2018', '1/24/2019', 7);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('sosharkeyh', 61.14, '4/17/2018', '11/6/2018', 3);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('iterrani', 219.14, '12/9/2018', '2/20/2019', 7);
+insert into Services (hostName, minBid, startdate, enddate, capacity) values ('cmottershawj', 669.57, '4/1/2018', '8/4/2018', 9);
