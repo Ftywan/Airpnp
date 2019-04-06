d9792a212ebc6127356a58039eb047a738bc430f
gaoqikai <gqk9000@outlook.com>
2019-04-06 18:21:46 +0800

    listings

--- App/routes/listings.js
+++ App/routes/listings.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
+// create a new pool
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'airpnp',
+  password: 'aaaa990730',
+  port: 5432,
+})
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('listings', { title: 'My listings' });
+var initial_query = "select * from Services";
+router.get('/', function (req, res, next) {
+  pool.query(initial_query, (err, data) => {
+    res.render('listings', { title: 'My listings', data: data.rows });
+  });
 });
 
 module.exports = router;
