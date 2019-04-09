e8d14ef4b493734c7027009e3c2eb73c2a492eba
qikai <gqk9000@outlook.com>
2019-04-09 17:55:35 +0800

    login nearly finished

--- App/routes/login.js
+++ App/routes/login.js
@@ -12,9 +12,13 @@ const pool = new Pool({
   port: 5432,
 })
 
+var check_login_query = "select username from users where location = 'logged in again';";
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('login', { title: 'Airpnp-Login' });
+  pool.query(check_login_query, (err, result) => {
+    console.log(result);
+    res.render('login', { title: 'Airpnp-Login', result: result.rows });
+  });
 });
 // router.get('/:user', function (req, res, next) {
 //   var user = req.params.user;
@@ -28,20 +32,30 @@ router.post('/', function (req, res, next) {
   const username = req.body.username;
   const password = sha256(req.body.password);
   var flag = 0;
-  var initial_query = "select username, password from users;"
+  var initial_query = "select username, password, location from users;"
+  var set_login_query = "update users set location = 'logged in again' where username = '" + username + "';";
+  console.log(set_login_query);
   pool.query(initial_query, (err, data) => {
     for (var i = 0; i < data.rows.length; i++) {
       if (data.rows[i]["username"] == username) {
         if (data.rows[i]["password"] == password) {
           flag = 1;
-          res.redirect('/account');
+          pool.query(set_login_query, (err, data) => {
+            res.redirect("/account");
+          });
+          console.log(data.rows[i]["location"]);
         }
       }
     }
     if (!flag) res.redirect('login');
   });
+});
 
-
-})
+//Log out
+router.post('/logout', function (req, res) {
+  var logout_query = "update users set location = 'logged out' where username = 'test';"
+  pool.query(logout_query, (err, data));
+  console.log("Logged out");
+});
 
 module.exports = router;
