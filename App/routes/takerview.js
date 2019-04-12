6d197e602332ce4f24c262d0284744e616eaddda
qikai <gqk9000@outlook.com>
2019-04-12 08:57:30 +0800

    done auto assign

--- App/routes/takerview.js
+++ App/routes/takerview.js
@@ -12,8 +12,10 @@ const pool = new Pool({
 })
 
 var login_query = "select username from login";
+var get_max_query = "select ownername, bids from biddingstatus where id =";
 // var get_a_id_query = "select max(id) from accommodation;";
 var username = '';
+var system_id;
 // var a_id = 0;
 
 function getQuery(req, res, next) {
@@ -26,17 +28,22 @@ function getQuery(req, res, next) {
         query = "update BiddingStatus set status='success' where ownerName='" + ownername + "' and id = " + id + " and status='pending'; update BiddingStatus set status='fail' where id=" + id + "and ownername <> '" + ownername + "' and status='pending'; insert into Accommodation values (" + id + ",'" + username + "', '" + ownername + "', 'sending', 10);";
         res.redirect('/history');
     }
-    return query;
 
+
+    if (process == "system") {
+
+        var id = req.param('id');
+        query = "update BiddingStatus set status='success' where ownerName='" + system_id + "' and id = " + id + " and status='pending'; update BiddingStatus set status='fail' where id=" + id + "and ownername <> '" + system_id + "' and status='pending'; insert into Accommodation values (" + id + ",'" + username + "', '" + system_id + "', 'sending', 10);";
+        console.log(query);
+        res.redirect('/history');
+    }
+
+    return query;
 }
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    // pool.query(get_a_id_query, (err, id_of_a) => {
-    //     if (id_of_a.rows.length) {
-    //         a_id = id_of_a.rows[0]["max"];
-    //         console.log(a_id);
-    //     }
+    
     pool.query(login_query, (err, result) => {
         var id = req.param("id");
         if (result.rows.length) {
@@ -44,8 +51,15 @@ router.get('/', function (req, res, next) {
         }
         var listing_query = "select * from biddingstatus where id = " + id + " and status = 'pending';";
         pool.query(listing_query, (err, data) => {
-            pool.query(getQuery(req, res, next), (err, no_use) => {
-                res.render('takerview', { title: 'All about pets', result: result.rows, data: data.rows });
+            var get_max_query_2 = get_max_query + id + " order by bids desc;";
+            console.log(get_max_query_2);
+            pool.query(get_max_query_2, (err, by_system) => {
+                if (by_system.rows.length) {
+                    system_id = by_system.rows[0]["ownername"];
+                }
+                pool.query(getQuery(req, res, next), (err, no_use) => {
+                    res.render('takerview', { title: 'All about pets', result: result.rows, data: data.rows });
+                });
             });
         });
     });
